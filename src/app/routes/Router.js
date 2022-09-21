fea588e3616cc45559c0e19185eca169f3cdf410
Yakir <yakirbar7820@gmail.com>
2022-09-21 15:20:47 +0300

    new

--- src/app/routes/Router.js
+++ src/app/routes/Router.js
@@ -3,20 +3,20 @@ import { Search, Details, Favorites } from '../pages';
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 const Router = (props) => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    const Unauthorized = () => {
-        useEffect(() => {
-            setTimeout(navigate('WeatherApp'));
-        }, []);
-    };
+    // const Unauthorized = () => {
+    //     useEffect(() => {
+    //         setTimeout(navigate('WeatherApp'));
+    //     }, []);
+    // };
 
     return (
         <Routes>
-            <Route path="*" element={<Unauthorized />} />
-            <Route path="/WeatherApp" element={<Search {...props} />} />
-            <Route path="/WeatherApp/favorites" element={<Favorites {...props} />} />
-            <Route path="/WeatherApp/:localizedName" element={<Details {...props} />} />
+            {/* <Route path="*" element={<Unauthorized />} /> */}
+            <Route path="/" element={<Search {...props} />} />
+            <Route path="/favorites" element={<Favorites {...props} />} />
+            <Route path="/:localizedName" element={<Details {...props} />} />
         </Routes>
     );
 };
