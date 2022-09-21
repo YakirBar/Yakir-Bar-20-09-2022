97a69b453dfc167b6234fa274e18a23dbe1ea662
Yakir <yakirbar7820@gmail.com>
2022-09-21 15:22:48 +0300

    fs

--- src/app/routes/Router.js
+++ src/app/routes/Router.js
@@ -3,20 +3,20 @@ import { Search, Details, Favorites } from '../pages';
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 const Router = (props) => {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
-    // const Unauthorized = () => {
-    //     useEffect(() => {
-    //         setTimeout(navigate('WeatherApp'));
-    //     }, []);
-    // };
+    const Unauthorized = () => {
+        useEffect(() => {
+            setTimeout(navigate('WeatherApp'));
+        }, []);
+    };
 
     return (
         <Routes>
-            {/* <Route path="*" element={<Unauthorized />} /> */}
-            <Route path="/" element={<Search {...props} />} />
-            <Route path="/favorites" element={<Favorites {...props} />} />
-            <Route path="/:localizedName" element={<Details {...props} />} />
+            <Route path="*" element={<Unauthorized />} />
+            <Route path="https://yakirbar.github.io/Yakir-Bar-20-09-2022" element={<Search {...props} />} />
+            <Route path="https://yakirbar.github.io/Yakir-Bar-20-09-2022/favorites" element={<Favorites {...props} />} />
+            <Route path="https://yakirbar.github.io/Yakir-Bar-20-09-2022/:localizedName" element={<Details {...props} />} />
         </Routes>
     );
 };
